f9f08d8ed95819ca84c6e1e71b703a88b489ebac
ShivaTejMatam <matamsivateja@gmail.com>
2025-04-29 22:34:16 +0530

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "html.tailus.io",
+        protocol: "https",
+        pathname: "/blocks/customers/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
